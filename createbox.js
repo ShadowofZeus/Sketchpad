824b3e3acf6636bcf642609d1bfaf14ac3f07e43
shadowofzeus <shadowofzeus811@gmail.com>
2020-02-11 21:18:10 +0200

    Added Color pixelating functionality

--- createbox.js
+++ createbox.js
@@ -1,4 +1,5 @@
 let form = document.querySelector('form');
+let littleDiv = document.getElementsByClassName('littleOnes');
 
 function createMyGrid(colNmbr)
 {
@@ -23,5 +24,20 @@ form.addEventListener('submit', e => {
 	createMyGrid(rows);
 
 	});
+
+document.querySelector("#box-section").addEventListener("mouseover", function( event ) { 
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);  
+  // highlight the mouseover target
+  event.target.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+  //console.log(event.target);
+
+ 
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.background = "";
+  }, 8000);
+}, false);
 
 
